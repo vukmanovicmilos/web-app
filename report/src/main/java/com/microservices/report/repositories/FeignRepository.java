807b3c3c27e3db746a55b0d3e4c48da6262fbae2
vukmanovicmilos <vukmanovic.milos@gmail.com>
2019-05-01 13:05:31 +0200

    netflix zuul api gateway

--- report/src/main/java/com/microservices/report/repositories/FeignRepository.java
+++ report/src/main/java/com/microservices/report/repositories/FeignRepository.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "faculty")
+@FeignClient(name="faculty")
 @RibbonClient(name="faculty")
 public interface FeignRepository {
 	
