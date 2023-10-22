0963627046c3f94f4dc56a72692585d6ff0ae727
Miloš Vukmanović <milos.vukmanovic@mi-system.rs>
2023-10-22 14:59:41 +0200

    create faculty ms

---
+++ faculty/src/main/java/com/microservices/faculty/FacultyApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.faculty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacultyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacultyApplication.class, args);
+	}
+
+}
---
+++ faculty/src/test/java/com/microservices/faculty/FacultyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.faculty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacultyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
