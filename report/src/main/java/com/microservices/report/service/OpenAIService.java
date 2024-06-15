7114db509ddbe5dc86533c4afcb623fd18c22a95
Miloš Vukmanović <vukmanovic.milos@gmail.com>
2024-06-16 01:25:43 +0200

    refactor for new version openAI

--- report/src/main/java/com/microservices/report/service/OpenAIService.java
+++ report/src/main/java/com/microservices/report/service/OpenAIService.java
@@ -1,80 +1,30 @@
 package com.microservices.report.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 @Service
-@RequiredArgsConstructor
 public class OpenAIService {
-    @Value("${openai.model}")
-    private String model;
 
-    @Value("${openai.api.url}")
-    private String apiUrl;
+    private final ChatClient chatClient;
 
-    @Value("${openai.api.key}")
-    private String key;
+    OpenAIService(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
 
     public String getSummary(Map<String, Object> params) {
-        String summary = "Default summary if openAI is not set or there is a problem";
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setBearerAuth(key);
-
-            String prompt = String.format("""
-                    Create short funny description for course name %s
-                    and course start date %s
-                    and teacher title %s
-                    and teacher last name %s
-                    """, params.get("courseName"), params.get("courseDate"), params.get("teacherTitle"), params.get("teacherLastName"));
-
-            List<Map<String, String>> messages = Arrays.asList(
-                    Map.of("role", "system", "content", "You are a helpful assistant."),
-                    Map.of("role", "user", "content", prompt)
-            );
-
-            Map<String, Object> requestBody = Map.of(
-                    "messages", messages,
-                    "max_tokens", 500,
-                    "model", model
-            );
-
-            HttpEntity<Map<String, Object>> requestEntity = new HttpEntity<>(requestBody, headers);
-
-            RestTemplate restTemplate = new RestTemplate();
-            ResponseEntity<String> response = restTemplate.exchange(
-                    apiUrl,
-                    HttpMethod.POST,
-                    requestEntity,
-                    String.class
-            );
-
-            if (response.getStatusCode().is2xxSuccessful()) {
-                ObjectMapper objectMapper = new ObjectMapper();
-                JsonNode jsonNode = objectMapper.readTree(response.getBody());
-
-                summary = jsonNode
-                        .path("choices")
-                        .path(0)
-                        .path("message")
-                        .path("content")
-                        .asText();
-            }
-        } catch (Exception e) {
-            // use some logger instead
-            System.out.println(e.getMessage());
-            return summary;
-        }
-        return summary;
+        System.out.println("BBBBBBBB" + chatClient);
+        String prompt = String.format("""
+                Create short funny description for course name %s
+                and course start date %s
+                and teacher title %s
+                and teacher last name %s
+                """, params.get("courseName"), params.get("courseDate"), params.get("teacherTitle"), params.get("teacherLastName"));
+        return this.chatClient.prompt()
+                .user(prompt)
+                .call()
+                .content();
     }
 }
